e5d2b61294092fb3036c88dbe774921e73cfa9a0
sithy <sithy@7bb9f1a6-b967-46cc-a5c0-e10e44e46661>
2009-01-04 21:11:41 +0000

    Fix packaging.
    git-svn-id: https://repos.curseforge.com/wow/rps/trunk@63 7bb9f1a6-b967-46cc-a5c0-e10e44e46661

--- RPOfficerSettings/Features.lua
+++ RPOfficerSettings/Features.lua
@@ -241,7 +241,7 @@ function RPOS:AddFeature(data)
 			if not self:RollListAdd(name, data.command) then
 				self:Whisper("You are already bidding on that item.", name)
 			else
-				self:Whisper(data.name .. " bid recieved." ,name)
+				self:Whisper(data.name .. " bid recieved.", name)
 			end
 		else
 			self:Whisper("No item is up for bidding.", name)
